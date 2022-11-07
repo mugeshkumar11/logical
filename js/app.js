0f657378ff9c9d81225780a3eb29a7d16f2cfa3b
mugeshkumar <kmugesh11199@gmail.com>
2022-11-07 19:58:51 +0530

    add new file

--- js/app.js
+++ js/app.js
@@ -225,19 +225,86 @@
 
 //prime number
 
-var input = 15;
-var number = true;
-if (input===1){
-  console.log("not a odd and even")
-}else if(input>1){
-for(let i=2; i<input; i++){
-  if(input % i === 0){
-    number = false;
-  }
-}
-}
-if(number===true){
-  console.log("its a prime number")
-}else{
-  console.log("its not prime number");
-}
+// var input = 15;
+// var number = true;
+// if (input===1){
+//   console.log("not a odd and even")
+// }else if(input>1){
+// for(let i=2; i<input; i++){
+//   if(input % i === 0){
+//     number = false;
+//   }
+// }
+// }
+// if(number===true){
+//   console.log("its a prime number")
+// }else{
+//   console.log("its not prime number");
+// }
+
+
+// program to print prime numbers between the two numbers
+
+// var input = 20;
+
+// for(let i=2; i<=input; i++){
+//    var rem = (i/2)
+//   var prime = true;
+//   for(let j=2; j<=rem; j++){
+//     if(i%j==0){
+//       prime = false
+//       break;
+//     }
+//   }
+
+//   if(prime==true){
+//     console.log("this is a prime",i);
+
+//   }else{
+//     console.log("not a prime number",i);
+//   }
+// }
+
+
+//vowels word 
+
+// var input = "mugeshkumar";
+// input = input.toLowerCase();
+// var count =0;
+// // console.log(input[1]);
+// for(let i=0; i<input.length; i++){
+//   if(input[i]=="a" ||input[i]=="e" ||input[i]=="i" ||input[i]=="o" ||input[i]=="u" ){
+//              count ++;
+//   }
+ 
+// }
+// console.log(count);
+
+
+//vowels
+
+// var input = "mugeshkumar";
+// var input2 = "sakthi";
+
+// var count = 0;
+// for(let i=0; i<input.length; i++){
+//   if(input[i]=="a" ||input[i]=="e" ||input[i]=="i" ||input[i]=="o" ||input[i]=="u" ){
+//                   count ++;
+//        }
+// }
+// console.log(count);
+// var count2 = 0;
+// for(let i=0; i<input2.length; i++){
+//   if(input2[i]=="a" ||input2[i]=="e" ||input2[i]=="i" ||input2[i]=="o" ||input2[i]=="u" ){
+//               count2 ++;  
+//       }
+// }
+// console.log(count2);
+
+// if(count>count2){
+//   console.log(input);
+// }else if(count<count2){
+//   console.log(input2);
+// }else{
+//   console.log(input,input2);
+// }
