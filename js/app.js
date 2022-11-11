caba36ed496259e4b193e2baa355f2c492bed37f
mugeshkumar <kmugesh11199@gmail.com>
2022-11-11 19:16:25 +0530

    add new file

--- js/app.js
+++ js/app.js
@@ -330,13 +330,29 @@
 
 // add and even number find and add
 
+// var input = 6789;
+// var sum = 0;
+// while(input>0){
+//  rem = input%10;
+//   input = (input-rem)/10;
+
+//   if(rem%2==0){
+//     sum += rem;
+   
+//   }
+  
+// }
+
+// console.log(sum);
+
+
 var input = 6789;
 var sum = 0;
 while(input>0){
  rem = input%10;
   input = (input-rem)/10;
 
-  if(rem%2==0){
+  if(rem%2!=0){
     sum += rem;
    
   }
